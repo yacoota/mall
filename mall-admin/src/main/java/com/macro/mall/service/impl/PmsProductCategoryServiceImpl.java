a06b49df5851063dc11d7a908d40f592a6684e7d
宇痴 <gao_20022002@126.com>
2020-12-20 07:54:52 +0800

    同步更新源代码，同步更新升级。

--- mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductCategoryServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/PmsProductCategoryServiceImpl.java
@@ -10,8 +10,7 @@ import com.macro.mall.mapper.PmsProductCategoryMapper;
 import com.macro.mall.mapper.PmsProductMapper;
 import com.macro.mall.model.*;
 import com.macro.mall.service.PmsProductCategoryService;
-// import org.apache.commons.collections.CollectionUtils;
-import org.springframework.util.CollectionUtils;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * PmsProductCategoryService实现类
+ * 商品分类管理Service实现类
  * Created by macro on 2018/4/26.
  */
 @Service
@@ -45,7 +44,7 @@ public class PmsProductCategoryServiceImpl implements PmsProductCategoryService 
         int count = productCategoryMapper.insertSelective(productCategory);
         //创建筛选属性关联
         List<Long> productAttributeIdList = pmsProductCategoryParam.getProductAttributeIdList();
-        if(!CollectionUtils.isEmpty(productAttributeIdList)){
+        if(!(CollectionUtils.size(productAttributeIdList)==0)){
             insertRelationList(productCategory.getId(), productAttributeIdList);
         }
         return count;
@@ -80,7 +79,7 @@ public class PmsProductCategoryServiceImpl implements PmsProductCategoryService 
         example.createCriteria().andProductCategoryIdEqualTo(id);
         productMapper.updateByExampleSelective(product,example);
         //同时更新筛选属性的信息
-        if(!CollectionUtils.isEmpty(pmsProductCategoryParam.getProductAttributeIdList())){
+        if(!(CollectionUtils.size(pmsProductCategoryParam.getProductAttributeIdList())==0)){
             PmsProductCategoryAttributeRelationExample relationExample = new PmsProductCategoryAttributeRelationExample();
             relationExample.createCriteria().andProductCategoryIdEqualTo(id);
             productCategoryAttributeRelationMapper.deleteByExample(relationExample);
